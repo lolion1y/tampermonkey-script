ac99f5157ed0b95d92a7732385aa4580a2f0944b
lolion1y <93830081+lolion1y@users.noreply.github.com>
2025-03-26 23:54:42 +0800

    feat: add config menu for `twitter`

--- twitter/clickcopylink.user.js
+++ twitter/clickcopylink.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         One Click Copy Link Button for Twitter(X)
 // @namespace    http://tampermonkey.net/
-// @version      2.3.3
+// @version      2.3.4
 // @description  Add a button to copy the URL of a tweet on Twitter without clicking dropdown. Default to twitter but customizable.
 // @author       lolion1y
 // @match        https://twitter.com/*
@@ -9,16 +9,50 @@
 // @match        https://tweetdeck.twitter.com/*
 // @match        https://x.com/*
 // @icon         https://www.google.com/s2/favicons?domain=twitter.com
-// @grant        none
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    const config = {
+    const defaultconfig = {
         like: true,
         retweet: true
-    }
+    };
+
+    const config = {
+        like: GM_getValue("like", defaultconfig.like),
+        retweet: GM_getValue("retweet", defaultconfig.retweet)
+    };
+
+    GM_registerMenuCommand(`Like (Current: ${config.like ? 'Enabled' : 'Disabled'})`, () => {
+        const userConfirm = confirm(`Do you want to ${config.like ? 'disable' : 'enable'} like?
+Current: ${config.like ? 'Enabled' : 'Disabled'}`);
+        if (userConfirm) {
+            config.like = !config.like;
+            if (config.like === defaultconfig.like) {
+                GM_deleteValue('like');
+            } else {
+                GM_setValue('like', config.like);
+            }
+        }
+    });
+
+    GM_registerMenuCommand(`Retweet (Current: ${config.retweet ? 'Enabled' : 'Disabled'})`, () => {
+        const userConfirm = confirm(`Do you want to ${config.retweet ? 'disable' : 'enable'} retweet?
+Current: ${config.retweet ? 'Enabled' : 'Disabled'}`);
+        if (userConfirm) {
+            config.retweet = !config.retweet;
+            if (config.retweet === defaultconfig.retweet) {
+                GM_deleteValue('retweet');
+            } else {
+                GM_setValue('retweet', config.retweet);
+            }
+        }
+    });
 
     const baseUrl = 'https://twitter.com';
     const defaultSVG = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard" viewBox="0 0 24 24" stroke-width="2" stroke="#71767C" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /></svg>';
--- twitter/origphoto.user.js
+++ twitter/origphoto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Twitter original images
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  View original quality images.
 // @author       lolion1y
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitter.com
@@ -9,16 +9,51 @@
 // @match        https://twitter.com/*
 // @match        https://mobile.twitter.com/*
 // @match        https://pbs.twimg.com/media/*
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_deleteValue
+// @grant        GM_registerMenuCommand
 // @run-at       document-end
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    const config = {
+    const defaultconfig = {
         fullsize: true,
         preview: false
-    }
+    };
+
+    const config = {
+        fullsize: GM_getValue("fullsize", defaultconfig.fullsize),
+        preview: GM_getValue("preview", defaultconfig.preview)
+    };
+
+    GM_registerMenuCommand(`Fullsize (Current: ${config.fullsize ? 'Enabled' : 'Disabled'})`, () => {
+        const userConfirm = confirm(`Do you want to ${config.fullsize ? 'disable' : 'enable'} fullsize?
+Current: ${config.fullsize ? 'Enabled' : 'Disabled'}`);
+        if (userConfirm) {
+            config.fullsize = !config.fullsize;
+            if (config.fullsize === defaultconfig.fullsize) {
+                GM_deleteValue('fullsize');
+            } else {
+                GM_setValue('fullsize', config.fullsize);
+            }
+        }
+    });
+
+    GM_registerMenuCommand(`Preview (Current: ${config.preview ? 'Enabled' : 'Disabled'})`, () => {
+        const userConfirm = confirm(`Do you want to ${config.preview ? 'disable' : 'enable'} preview?
+Current: ${config.preview ? 'Enabled' : 'Disabled'}`);
+        if (userConfirm) {
+            config.preview = !config.preview;
+            if (config.preview === defaultconfig.preview) {
+                GM_deleteValue('preview');
+            } else {
+                GM_setValue('preview', config.preview);
+            }
+        }
+    });
 
     if (config.fullsize || config.preview) {
 
