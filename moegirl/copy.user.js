aba377b6081014ace721e9419f0005eb13086836
lolion1y <93830081+lolion1y@users.noreply.github.com>
2025-05-06 22:48:42 +0800

    chore: `moegirl`

--- moegirl/copy.user.js
+++ moegirl/copy.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         moegirl copy
 // @namespace    http://tampermonkey.net/
-// @version      0.721
+// @version      1.0
 // @description  moegirl copy
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zh.moegirl.org.cn
 // @author       lolion1y
 // @match        *://*.moegirl.org.cn/*
 // @grant        none
@@ -10,7 +11,5 @@
 
 (function () {
     'use strict';
-
     document.addEventListener('copy', function (event) { event.stopPropagation(); }, true);
-
 })();
