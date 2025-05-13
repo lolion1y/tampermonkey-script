8770b65a73aa9073928ca2ed7b2d5d9db3048a84
lolion1y <93830081+lolion1y@users.noreply.github.com>
2025-05-13 23:44:12 +0800

    chore: `moegirl`

--- moegirl/copy.user.js
+++ moegirl/copy.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name         moegirl copy
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  moegirl copy
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=zh.moegirl.org.cn
 // @author       lolion1y
 // @match        *://*.moegirl.org.cn/*
+// @match        *://*.juejin.cn/*
 // @grant        none
 // ==/UserScript==
 
