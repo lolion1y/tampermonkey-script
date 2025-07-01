3e7668eff5dc166ec74c69bb256ff716769a6104
lolion1y <93830081+lolion1y@users.noreply.github.com>
2025-07-01 17:30:13 +0800

    ref: `clickcopylink`

--- twitter/clickcopylink.user.js
+++ twitter/clickcopylink.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         One Click Copy Link Button for Twitter(X)
 // @namespace    http://tampermonkey.net/
-// @version      2.3.5
+// @version      2.3.6
 // @description  Add a button to copy the URL of a tweet on Twitter without clicking dropdown. Default to twitter but customizable.
 // @author       lolion1y
 // @match        https://twitter.com/*
@@ -55,8 +55,8 @@ Current: ${config.retweet ? 'Enabled' : 'Disabled'}`);
     });
 
     const baseUrl = 'https://twitter.com';
-    const defaultSVG = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard" viewBox="0 0 24 24" stroke-width="2" stroke="#71767C" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /></svg>';
-    const copiedSVG = '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard-check" viewBox="0 0 24 24" stroke-width="2" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2" /><path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /><path d="M9 14l2 2l4 -4" /></svg>';
+    const defaultSVG = '<svg class="icon icon-tabler icon-tabler-clipboard" viewBox="0 0 24 24" width="1.25em" height="1.25em" stroke-width="2" stroke="#71767C" fill="none" stroke-linecap="round" stroke-linejoin="round"><g><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2 M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z" /></g></svg>';
+    const copiedSVG = '<svg class="icon icon-tabler icon-tabler-clipboard-check" viewBox="0 0 24 24" width="1.25em" height="1.25em" stroke-width="2" stroke="#00abfb" fill="none" stroke-linecap="round" stroke-linejoin="round"><g><path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2 M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z M9 14l2 2l4 -4" /></g></svg>';
 
     function addCopyButtonToTweets() {
         const tweets = document.querySelectorAll('button[data-testid="like"], button[data-testid="unlike"]');
@@ -69,8 +69,7 @@ Current: ${config.retweet ? 'Enabled' : 'Disabled'}`);
                 copyIcon.classList.add('custom-copy-icon');
                 copyIcon.setAttribute('aria-label', 'Copy link');
                 copyIcon.setAttribute('role', 'button');
-                copyIcon.setAttribute('tabindex', '0');
-                copyIcon.style.cssText = 'display: flex; align-items: center; justify-content: center; width: 19px; height: 19px; border-radius: 9999px; transition-duration: 0.2s; cursor: pointer;';
+                copyIcon.style.cssText = 'display: flex; align-items: center; justify-content: center; border-radius: 9999px; transition-duration: 0.2s; cursor: pointer; padding: 0px 8px; margin: 0px -8px;';
                 copyIcon.innerHTML = defaultSVG;
 
                 copyIcon.addEventListener('click', (event) => {
@@ -81,20 +80,14 @@ Current: ${config.retweet ? 'Enabled' : 'Disabled'}`);
                             .then(() => {
                                 console.log('Tweet link copied!');
                                 copyIcon.innerHTML = copiedSVG;
-
                                 if (config.like) {
                                     const likeButton = tweet.querySelector('button[data-testid="like"]');
-                                    if (likeButton) {
-                                        likeButton.click();
-                                        console.log('Tweet liked!');
-                                    }
+                                    likeButton.click();
                                 }
-
                                 if (config.retweet) {
                                     const retweetButton = tweet.querySelector('button[data-testid="retweet"]');
-                                    if (retweetButton && tweet.querySelector('button[data-testid="like"]')) {
+                                    if (tweet.querySelector('button[data-testid="like"]')) {
                                         retweetButton.click();
-                                        console.log('Tweet retweeted!');
                                     }
                                 }
 
@@ -104,7 +97,6 @@ Current: ${config.retweet ? 'Enabled' : 'Disabled'}`);
                 });
 
                 const parentDivClone = parentDiv.cloneNode(true);
-                parentDivClone.style.cssText = 'display: flex !important; align-items: center;';
                 parentDiv.parentNode.insertBefore(parentDivClone, parentDiv.nextSibling);
                 parentDivClone.innerHTML = '';
                 parentDivClone.appendChild(copyIcon);
